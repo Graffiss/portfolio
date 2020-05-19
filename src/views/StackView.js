0f63229d6d9e759e4176c6d4dadf20ccaab547d5
Graffiss <graffis@gmail.com>
2020-05-19 15:42:25 +0200

    zmiany w stack view

--- src/views/StackView.js
+++ src/views/StackView.js
@@ -15,12 +15,12 @@ const StyledWrapper = styled.div`
 
 const StyledGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 85px;
 
   @media (max-width: 1500px) {
     grid-gap: 45px;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(2, 1fr);
   }
   @media (max-width: 1100px) {
     grid-template-columns: 1fr 1fr;
