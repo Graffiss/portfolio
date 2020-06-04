9765867979ebc30e5878f600aaeed1fc493c53e5
Graffiss <graffis@gmail.com>
2020-06-04 10:28:41 +0200

    dodano projekt Mood-Wallpapers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
-import organicMarketPhoto from '../assets/images/projects/organic-food-market-project.png';
+// import organicMarketPhoto from '../assets/images/projects/organic-food-market-project.png';
+
 import portfolioGif from '../assets/gifs/projects/portfolio.gif';
 import homePantryGif from '../assets/gifs/projects/home-pantry.gif';
 import hangmanGif from '../assets/gifs/projects/hangman-game.gif';
+import wallpapersGif from '../assets/gifs/projects/mood-wallpapers.gif';
 
 const initialState = {
   projects: [
@@ -26,17 +28,6 @@ const initialState = {
     },
     {
       id: 2,
-      image: homePantryGif,
-      title: 'Domowa spiżarnia',
-      desc: `Projekt domowej spiżarni jest oparty o design w Material.UI. Spiżarnia pozwala sprawdzić stan
-      produktów dostępnych w domu, a wszelkie aktualizacje zapisują się w localStorage. Zakładka "Lista zakupów"
-      aktualizuje się automatycznie, gdy ilość produktów równa jest minimalnej ilości, jaką ustawiliśmy.`,
-      stack: ['react', 'redux', 'material.ui', 'localStorage'],
-      github: 'https://github.com/Graffiss/home-pantry',
-      demo: 'https://home-pantry.netlify.app/',
-    },
-    {
-      id: 3,
       image: hangmanGif,
       title: 'Hangman Game',
       desc: `Jedno ze starych zadań rekrutacyjnych w Netguru: gra w wisielca. Sterowanie za pomocą klawiatury.
@@ -46,6 +37,28 @@ const initialState = {
       demo: 'https://hangman-game-recruitment-task.netlify.app/',
     },
     {
+      id: 3,
+      image: wallpapersGif,
+      title: 'Mood wallpapers',
+      desc: `Aplikacja najpierw sprawdza Geolokalizację użytkownika, następnie odpytuje dwa kolejne API: DarkSky API w celu sprawdzenia pogody oraz
+      Reverse Geolocation API w celu sprawdzenia miasta, w którym znajduje się użytkownik. Na podstawie tych dwóch API, odpytuje trzecie, Unsplash API w celu
+      zaproponowania odpowiednich tapet. Wyszukiwarka w prawym górnym rogu pozwala również na wyszukiwanie po własnych hasłach`,
+      stack: ['react', 'scss modules', 'API', 'hooks', 'contextAPI', 'geolocation'],
+      github: 'https://github.com/Graffiss/mood-wallpapers',
+      demo: 'https://mood-wallpapers.netlify.app/',
+    },
+    {
+      id: 4,
+      image: homePantryGif,
+      title: 'Domowa spiżarnia',
+      desc: `Projekt domowej spiżarni jest oparty o design w Material.UI. Spiżarnia pozwala sprawdzić stan
+                          produktów dostępnych w domu, a wszelkie aktualizacje zapisują się w localStorage. Zakładka "Lista zakupów"
+                          aktualizuje się automatycznie, gdy ilość produktów równa jest minimalnej ilości, jaką ustawiliśmy.`,
+      stack: ['react', 'redux', 'material.ui', 'localStorage'],
+      github: 'https://github.com/Graffiss/home-pantry',
+      demo: 'https://home-pantry.netlify.app/',
+    },
+    /* {
       id: 4,
       image: organicMarketPhoto,
       title: 'Organic Food Market',
@@ -53,7 +66,7 @@ const initialState = {
       stack: ['react', 'redux', 'gatsby', 'graphql'],
       github: 'https://github.com/Graffiss/organic-food-market',
       demo: 'https://organic-food-market.netlify.app/',
-    },
+    }, */
   ],
 };
 
